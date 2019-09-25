ef468c653594f6c738cf3d846397e157a4021b7c
atul <atulk19966@gmail.com>
2019-09-25 09:31:37 +0530

    third commit

---
+++ src/main/java/com/stackroute/domain/Main.java
@@ -0,0 +1,18 @@
+package com.stackroute.domain;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+public class Main {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+        System.out.println("Config file loaded.");
+        movie movie1 = context.getBean(movie.class);
+        System.out.println(movie1.getActor().getName());
+        System.out.println(movie1.getActor().getGender());
+        System.out.println(movie1.getActor().getAge());
+
+
+
+    }
+}
+
